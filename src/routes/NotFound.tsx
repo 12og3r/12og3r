d11c0321769cd91afdc2a7a4ec8e2f1a7960285d
Roger <roger@Rogers-MacBook-Air.local>
2026-05-02 18:50:36 +0800

    feat: add 404 page in terminal style

--- src/routes/NotFound.tsx
+++ src/routes/NotFound.tsx
@@ -1,3 +1,22 @@
+import { Link, useLocation } from 'react-router-dom';
+import { TopBar } from '@/components/TopBar';
+import { useI18n } from '@/hooks/useI18n';
+import './NotFound.css';
+
 export default function NotFound() {
-  return <div data-testid="notfound">404</div>;
+  const { pathname } = useLocation();
+  const { lang, t } = useI18n();
+  return (
+    <>
+      <TopBar />
+      <main className="notfound">
+        <pre className="cmd">{t('notfound.cmd', { path: pathname })}</pre>
+        <div className="suggest">{t('notfound.suggest')}</div>
+        <ul>
+          <li><Link to={`/${lang}`}>$ cd ~ &nbsp;&nbsp;{t('notfound.home')}</Link></li>
+          <li><Link to={`/${lang}`}>$ ls posts/ &nbsp;&nbsp;{t('notfound.browse')}</Link></li>
+        </ul>
+      </main>
+    </>
+  );
 }
